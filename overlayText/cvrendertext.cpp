1c6b8f5e7f74f24e8a5f16616efd19f1efff5563
HungNV <hung.viet.nguyen.hp@gmail.com>
2018-08-02 23:20:00 +0700

    Added border effect. Make more samples in main

--- overlayText/cvrendertext.cpp
+++ overlayText/cvrendertext.cpp
@@ -10,7 +10,11 @@
 #endif
 
 CVRenderText::CVRenderText()
-{
+	: mLibrary(NULL)
+	, mStroker(NULL)
+	, mFace(NULL)
+	, mInitialized(false)
+	, mFontName("") {
 	FT_Error error;
 	error = FT_Init_FreeType(&mLibrary);
 
@@ -19,14 +23,25 @@ CVRenderText::CVRenderText()
 	else
 		mInitialized = false;
 
+	error = FT_Stroker_New(mLibrary, &mStroker);
+	if (error || !mStroker) {
+		mInitialized = false;
+		FT_Done_FreeType(mLibrary);
+		mLibrary = NULL;
+	}
+
 	mFace = NULL;
 }
 
-CVRenderText::~CVRenderText()
-{
+CVRenderText::~CVRenderText() {
 	if (mFace) {
 		FT_Done_Face(mFace);
 		mFace = NULL;
+	}
+
+	if (mStroker) {
+		FT_Stroker_Done(mStroker);
+		mStroker = NULL;
 	}
 
 	if (mLibrary) {
@@ -35,8 +50,7 @@ CVRenderText::~CVRenderText()
 	}
 }
 
-int CVRenderText::setFont(const char* path_to_font)
-{
+int CVRenderText::setFont(const char* path_to_font) {
 	FT_Error error;
 	mFontName = path_to_font;
 
@@ -49,22 +63,32 @@ int CVRenderText::setFont(const char* path_to_font)
 		error = FT_Init_FreeType(&mLibrary);
 		if (error != 0)
 			return error;
+
+		error = FT_Stroker_New(mLibrary, &mStroker);
+		if (error != 0 || !mStroker) {
+			FT_Done_FreeType(mLibrary);
+			mLibrary = NULL;
+			return error;
+		}
 	}
 
 	return FT_New_Face(mLibrary, path_to_font, 0, &mFace);
 }
 
 int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text, size_t textSize, Justify xMargin, Justify yMargin, 
-							 cv::Scalar textColor, bool hasBackgrnd, cv::Scalar bgrColor, double bgrOpacity)
+		cv::Scalar textColor, bool hasBorder, size_t brdSize, cv::Scalar brdColor, bool hasBackgrnd, cv::Scalar bgrColor, double bgrOpacity)
 {
 	FT_Error error;
 
 	if (!mFace)
 		return -1;
 
-	error = FT_Set_Pixel_Sizes(mFace, textSize, textSize);
+	error = FT_Set_Char_Size(mFace, 0, textSize * 64, 0, 0);
 	if (error != 0)
 		return error;
+
+	if (hasBorder && mStroker)
+		FT_Stroker_Set(mStroker, brdSize * 64, FT_STROKER_LINECAP_ROUND, FT_STROKER_LINEJOIN_ROUND, 0);
 
 	// Get total width
 	unsigned int total_width = 0;
@@ -79,7 +103,7 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 		// Have to use FT_LOAD_RENDER.
 		// If use FT_LOAD_DEFAULT, the actual glyph bitmap won't be loaded,
 		// thus bitmap->rows will be incorrect, causing insufficient max_height.
-		error = FT_Load_Glyph(mFace, glyph_index, FT_LOAD_RENDER);
+		error = FT_Load_Glyph(mFace, glyph_index, FT_LOAD_DEFAULT);
 		if (error != 0) {
 			return error;
 		}
@@ -89,9 +113,17 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 			return error;
 		}
 
-		FT_Glyph_Get_CBox( glyph, FT_GLYPH_BBOX_PIXELS, &bbox );
+		if (hasBorder) {
+			FT_Glyph_StrokeBorder(&glyph, mStroker, false, true);
+		}
+
+		FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_NORMAL, nullptr, true);
+		FT_Glyph_Get_CBox( glyph, FT_GLYPH_BBOX_TRUNCATE, &bbox );
+		FT_BitmapGlyph bitmapGlyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
 		
 		total_width += std::max(bbox.xMax, mFace->glyph->advance.x >> 6);
+		if (hasBorder)
+			total_width += brdSize;
 
 		max_top = std::max(max_top, bbox.yMax + 1);
 		min_bottom = std::min(min_bottom, bbox.yMin - 1);
@@ -101,14 +133,15 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 	max_height = (unsigned int)(max_top - min_bottom);
 
 	// Copy grayscale image from FreeType to OpenCV
-	cv::Mat gray(max_height, total_width, CV_8UC1, cv::Scalar::all(0));
+	cv::Mat gray_outline(max_height, total_width, CV_8UC1, cv::Scalar::all(0));
+	cv::Mat gray_text(max_height, total_width, CV_8UC1, cv::Scalar::all(0));
 	int x = 0;
 	for (int i = 0; i < std::wcslen(text); i++) {
 		FT_UInt glyph_index = FT_Get_Char_Index(mFace, text[i]);
 		FT_BBox  bbox;
 		FT_Glyph glyph;
 
-		error = FT_Load_Glyph(mFace, glyph_index, FT_LOAD_RENDER);
+		error = FT_Load_Glyph(mFace, glyph_index, FT_LOAD_DEFAULT);
 		if (error != 0) {
 			return error;
 		}
@@ -118,22 +151,71 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 			return error;
 		}
 
-		FT_Glyph_Get_CBox( glyph, FT_GLYPH_BBOX_PIXELS, &bbox );
+		if (hasBorder) {
+			// create outline
+			FT_Glyph_StrokeBorder(&glyph, mStroker, false, true);
+			FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_NORMAL, nullptr, true);
+			FT_Glyph_Get_CBox( glyph, FT_GLYPH_BBOX_TRUNCATE, &bbox );
 
-		FT_Bitmap *bitmap = &(mFace->glyph->bitmap);
-		cv::Mat glyph_img(bitmap->rows, bitmap->width, CV_8UC1, bitmap->buffer, bitmap->pitch);
-		int top = max_top - bbox.yMax;
+			FT_BitmapGlyph bitmapGlyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
+			cv::Mat glyph_img(bitmapGlyph->bitmap.rows, bitmapGlyph->bitmap.width, CV_8UC1, bitmapGlyph->bitmap.buffer, bitmapGlyph->bitmap.pitch);
+			int top = max_top - bbox.yMax;
 
-		int left = x + mFace->glyph->bitmap_left;
-		if (left < 0)
-			left = 0;
-		cv::Rect rect(left, top, bitmap->width, bitmap->rows);
+			int left = x + bitmapGlyph->left;
+			if (left < 0)
+				left = 0;
+			cv::Rect rect(left, top, bitmapGlyph->bitmap.width, bitmapGlyph->bitmap.rows);
 
-		cv::Mat gray_part(gray, rect);
-		glyph_img.copyTo(gray_part);
+			cv::Mat gray_part(gray_outline, rect);
+			glyph_img.copyTo(gray_part);
 
-		x += std::max(bbox.xMax, mFace->glyph->advance.x >> 6);
-		FT_Done_Glyph(glyph);
+			int max_x = std::max(bbox.xMax, mFace->glyph->advance.x >> 6);
+			
+			FT_Done_Glyph(glyph);
+			//create text
+			error = FT_Get_Glyph(mFace->glyph, &glyph);
+			if (error != 0) {
+				return error;
+			}
+
+			FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_NORMAL, nullptr, true);
+			FT_Glyph_Get_CBox( glyph, FT_GLYPH_BBOX_TRUNCATE, &bbox );
+
+			bitmapGlyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
+			glyph_img = cv::Mat(bitmapGlyph->bitmap.rows, bitmapGlyph->bitmap.width, CV_8UC1, bitmapGlyph->bitmap.buffer, bitmapGlyph->bitmap.pitch);
+			top = max_top - bbox.yMax;
+
+			left = x + bitmapGlyph->left;
+			if (left < 0)
+				left = 0;
+			rect = cv::Rect(left, top, bitmapGlyph->bitmap.width, bitmapGlyph->bitmap.rows);
+
+			gray_part = cv::Mat(gray_text, rect);
+			glyph_img.copyTo(gray_part);
+
+			x += max_x;
+			if (hasBorder)
+				x += brdSize;
+			FT_Done_Glyph(glyph);
+		} else {
+			FT_Glyph_To_Bitmap(&glyph, FT_RENDER_MODE_NORMAL, nullptr, true);
+			FT_Glyph_Get_CBox( glyph, FT_GLYPH_BBOX_PIXELS, &bbox );
+
+			FT_BitmapGlyph bitmapGlyph = reinterpret_cast<FT_BitmapGlyph>(glyph);
+			cv::Mat glyph_img(bitmapGlyph->bitmap.rows, bitmapGlyph->bitmap.width, CV_8UC1, bitmapGlyph->bitmap.buffer, bitmapGlyph->bitmap.pitch);
+			int top = max_top - bbox.yMax;
+
+			int left = x + bitmapGlyph->left;
+			if (left < 0)
+				left = 0;
+			cv::Rect rect(left, top, bitmapGlyph->bitmap.width, bitmapGlyph->bitmap.rows);
+
+			cv::Mat gray_part(gray_outline, rect);
+			glyph_img.copyTo(gray_part);
+
+			x += std::max(bbox.xMax, mFace->glyph->advance.x >> 6);
+			FT_Done_Glyph(glyph);
+		}
 	}
 
 	// re-calculate position to render text over destination image
@@ -179,25 +261,43 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 	// rebuild ROI of image text overlayed in case of out of destination image
 	cv::Rect rectText(0, 0, width, height);
 
-	cv::Mat gray_bgr(gray.size(), CV_8UC1, cv::Scalar::all(255));
-	cv::Mat gray_img(gray.size(), CV_8UC1, cv::Scalar::all(255));
-	cv::Mat text_clr(gray.size(), CV_8UC3, textColor);
-	cv::Mat bgrd_clr(gray.size(), CV_8UC3, bgrColor);
-	cv::Mat coeffMat(gray.size(), CV_32FC3);
+	cv::Mat gray_bgr(gray_outline.size(), CV_8UC1, cv::Scalar::all(255));
+	cv::Mat gray_img(gray_outline.size(), CV_8UC1, cv::Scalar::all(255));
+	cv::Mat outline_clr(gray_outline.size(), CV_8UC3, brdColor);
+	cv::Mat text_clr(gray_text.size(), CV_8UC3, textColor);
+	cv::Mat bgrd_clr(gray_outline.size(), CV_8UC3, bgrColor);
+	cv::Mat coeffMat(gray_outline.size(), CV_32FC3);
 	std::vector<cv::Mat> coeff;
 
 	// calculate gray for background
-	cv::subtract(gray_bgr, gray, gray_bgr);
+	cv::subtract(gray_bgr, gray_outline, gray_bgr);
 	// calculate gray for image
-	cv::subtract(gray_img, gray, gray_img);
+	cv::subtract(gray_img, gray_outline, gray_img);
+	
+	if (hasBorder) {
+		// fill outline color
+		coeff.push_back(gray_outline);
+		coeff.push_back(gray_outline);
+		coeff.push_back(gray_outline);
 
-	// fill text color
-	coeff.push_back(gray);
-	coeff.push_back(gray);
-	coeff.push_back(gray);
+		cv::merge(coeff, coeffMat);
+		cv::multiply(coeffMat, outline_clr, outline_clr, 1.0/255.0);
+		// fill text color
+		coeff.clear();
+		coeff.push_back(gray_text);
+		coeff.push_back(gray_text);
+		coeff.push_back(gray_text);
+		cv::merge(coeff, coeffMat);
+		cv::multiply(coeffMat, text_clr, text_clr, 1.0/255.0);
+	} else {
+		// fill text color
+		coeff.push_back(gray_outline);
+		coeff.push_back(gray_outline);
+		coeff.push_back(gray_outline);
 
-	cv::merge(coeff, coeffMat);
-	cv::multiply(coeffMat, text_clr, text_clr, 1.0/255.0);
+		cv::merge(coeff, coeffMat);
+		cv::multiply(coeffMat, text_clr, text_clr, 1.0/255.0);
+	}
 
 	if (hasBackgrnd) {
 		// normalize opacity
@@ -219,6 +319,7 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 		cv::multiply(coeffMat, bgrd_clr, bgrd_clr, 1.0/255.0);
 	
 		cv::Mat blendImg(dstImg, rect);
+		cv::Mat blendOutline(outline_clr, rectText);
 		cv::Mat blendText(text_clr, rectText);
 		cv::Mat blendBgrd(bgrd_clr, rectText);
 
@@ -231,11 +332,27 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 		cv::multiply(coeffMat, blendImg, blendImg, 1.0/255.0);
 
 		// now merge text to image
-		cv::add(blendImg, blendText, blendImg);
+		if (hasBorder) {
+			cv::add(blendImg, blendOutline, blendImg);
+
+			gray_img = cv::Mat(gray_text.size(), CV_8UC1, cv::Scalar::all(255));
+			cv::subtract(gray_img, gray_text, gray_img);
+			coeff.clear();
+			coeff.push_back(gray_img(rectText));
+			coeff.push_back(gray_img(rectText));
+			coeff.push_back(gray_img(rectText));
+			cv::merge(coeff, coeffMat);
+			cv::multiply(coeffMat, blendImg, blendImg, 1.0/255.0);
+			cv::add(blendImg, blendText, blendImg);
+		} else {
+			cv::add(blendImg, blendText, blendImg);
+		}
+
 		cv::add(blendImg, blendBgrd, blendImg);
 	}
 	else {
 		cv::Mat blendImg(dstImg, rect);
+		cv::Mat blendOutline(outline_clr, rectText);
 		cv::Mat blendText(text_clr, rectText);
 
 		// build overlayed ROI of destination image with gray scale
@@ -247,7 +364,20 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 		cv::multiply(coeffMat, blendImg, blendImg, 1.0/255.0);
 
 		// now merge text to image
-		cv::add(blendImg, blendText, blendImg);
+		if (hasBorder) {
+			cv::add(blendImg, blendOutline, blendImg);
+			gray_img = cv::Mat(gray_text.size(), CV_8UC1, cv::Scalar::all(255));
+			cv::subtract(gray_img, gray_text, gray_img);
+			coeff.clear();
+			coeff.push_back(gray_img(rectText));
+			coeff.push_back(gray_img(rectText));
+			coeff.push_back(gray_img(rectText));
+			cv::merge(coeff, coeffMat);
+			cv::multiply(coeffMat, blendImg, blendImg, 1.0/255.0);
+			cv::add(blendImg, blendText, blendImg);
+		} else {
+			cv::add(blendImg, blendText, blendImg);
+		}
 	}
 
 	
@@ -256,11 +386,11 @@ int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text
 }
 
 int CVRenderText::renderText(cv::Mat &dstImg, cv::Point pos, const char* text, size_t textSize, Justify xMargin, Justify yMargin, 
-							 cv::Scalar textColor, bool hasBackgrnd, cv::Scalar bgrColor, double bgrOpacity)
+		cv::Scalar textColor, bool hasBorder, size_t brdSize, cv::Scalar brdColor, bool hasBackgrnd, cv::Scalar bgrColor, double bgrOpacity)
 {
 	std::string mb(text);
 	std::wstring ws(mb.size(), L' ');
 
 	ws.resize(std::mbstowcs(&ws[0], mb.c_str(), mb.size()));
-	return renderText(dstImg, pos, ws.c_str(), textSize, xMargin, yMargin, textColor, hasBackgrnd, bgrColor, bgrOpacity);
+	return renderText(dstImg, pos, ws.c_str(), textSize, xMargin, yMargin, textColor, hasBorder, brdSize, brdColor, hasBackgrnd, bgrColor, bgrOpacity);
 }
--- overlayText/cvrendertext.h
+++ overlayText/cvrendertext.h
@@ -4,6 +4,7 @@
 // FreeType headers
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_STROKER_H
 
 #include <cstring>
 
@@ -14,6 +15,7 @@ class CVRenderText
 {
 protected:
 	FT_Library mLibrary;
+	FT_Stroker mStroker;
 	FT_Face mFace;
 	bool mInitialized;
 	std::string mFontName;
@@ -32,10 +34,10 @@ public:
 	int setFont(const char* path_to_font);
 
 	int renderText(cv::Mat &dstImg, cv::Point pos, const wchar_t* text, size_t textSize, Justify xMargin = CENTER_MARGIN, Justify yMargin = CENTER_MARGIN, 
-		cv::Scalar textColor = cv::Scalar::all(255), bool hasBackgrnd = true, cv::Scalar bgrColor = cv::Scalar::all(0), double bgrOpacity = 0.0);
+		cv::Scalar textColor = cv::Scalar::all(255), bool hasBorder = true, size_t brdSize = 2, cv::Scalar brdColor = cv::Scalar::all(0), bool hasBackgrnd = true, cv::Scalar bgrColor = cv::Scalar::all(0), double bgrOpacity = 0.0);
 
 	int renderText(cv::Mat &dstImg, cv::Point pos, const char* text, size_t textSize, Justify xMargin = CENTER_MARGIN, Justify yMargin = CENTER_MARGIN, 
-		cv::Scalar textColor = cv::Scalar::all(255), bool hasBackgrnd = true, cv::Scalar bgrColor = cv::Scalar::all(0), double bgrOpacity = 0.0);
+		cv::Scalar textColor = cv::Scalar::all(255), bool hasBorder = true, size_t brdSize = 2, cv::Scalar brdColor = cv::Scalar::all(0), bool hasBackgrnd = true, cv::Scalar bgrColor = cv::Scalar::all(0), double bgrOpacity = 0.0);
 };
 
 #endif//CV_RENDER_TEXT_H__
